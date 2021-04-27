165bf6e015f99b60056442905f1e9b24a4e7ffff
Vishav Ranta <48310617+rantavishav@users.noreply.github.com>
2021-04-27 18:35:31 +0530

    first commit

---
+++ src/product/product.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    sku: string;
+
+    @Column()
+    price: number;
+}
